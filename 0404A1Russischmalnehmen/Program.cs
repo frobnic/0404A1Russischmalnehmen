0d11c83ad40d1b3d23be778241257435513ca1d2
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 15:48:21 +0200

    inserted code

--- 0404A1Russischmalnehmen/Program.cs
+++ 0404A1Russischmalnehmen/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0404A1Russischmalnehmen
 {
@@ -10,6 +6,28 @@ namespace _0404A1Russischmalnehmen
     {
         static void Main(string[] args)
         {
+            int a, b;
+            int r = 0;
+
+            Console.Write("a :");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("b :");
+            b = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 1)
+                r = b;
+
+            do
+            {
+                a = a / 2;
+                b = b * 2;
+                if (a % 2 == 1)
+                    r = r + b;
+                Console.WriteLine("a: {0,4}  b: {1,4}  r: {2,4}", a, b, r);
+            } while (a > 1);
+
+
         }
     }
 }

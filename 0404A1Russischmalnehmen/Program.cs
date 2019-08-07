6f279f10ab4f65ec42f9190431505b510598b7b0
frank paulsen <frobnic+ita@gmail.com>
2019-08-07 08:37:15 +0200

    added input checks for a and b

--- 0404A1Russischmalnehmen/Program.cs
+++ 0404A1Russischmalnehmen/Program.cs
@@ -15,19 +15,20 @@ namespace _0404A1Russischmalnehmen
             Console.Write("b :");
             b = int.Parse(Console.ReadLine());
 
-            if (a % 2 == 1)
-                r = b;
-
-            do
+            if (a > 0 && b > 0)
             {
-                a = a / 2;
-                b = b * 2;
                 if (a % 2 == 1)
-                    r = r + b;
-                Console.WriteLine("a: {0,4}  b: {1,4}  r: {2,4}", a, b, r);
-            } while (a > 1);
+                    r = b;
 
-
+                do
+                {
+                    a = a / 2;
+                    b = b * 2;
+                    if (a % 2 == 1)
+                        r = r + b;
+                    Console.WriteLine("a: {0,4}  b: {1,4}  r: {2,4}", a, b, r);
+                } while (a > 1);
+            }
         }
     }
 }
